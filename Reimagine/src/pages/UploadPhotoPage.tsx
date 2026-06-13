df4fd7c7941587ec6c2184458bc2a7436d086579
Matthew Drake <matthewdrake2004@gmail.com>
2026-06-13 15:00:24 -0500

    Mocked up basic skeleton of Upload photo page

--- Reimagine/src/pages/UploadPhotoPage.tsx
+++ Reimagine/src/pages/UploadPhotoPage.tsx
@@ -1,5 +1,5 @@
-import { IonButton, IonButtons, IonContent, IonHeader, IonMenu, IonMenuButton, IonPage, IonSelect, IonSelectOption, IonTitle, IonToolbar } from '@ionic/react';
-import DropdownMenu from '../components/DropdownMenu';
+import { IonButton, IonButtons, IonContent, IonHeader, IonItem, IonList, IonMenu, IonMenuButton, IonPage, IonSelect, IonSelectOption, IonTitle, IonToolbar } from '@ionic/react';
+import "./UploadPhotoPage.css"
 
 const UploadPhotoPage: React.FC = () => {
   return (
@@ -28,38 +28,54 @@ const UploadPhotoPage: React.FC = () => {
           <IonContent fullscreen>
     
             {/* Content */}
-            <div style={{display: 'flex' , flexDirection : 'column', alignItems: 'center', width: '100vw', height: '100vh'}}>
+            <div id="content-div">
     
-                <div style={{
-                    display : 'flex', 
-                    flexDirection : 'column', 
-                    width : '95%', 
-                    alignItems : 'center',
-                    backgroundColor : 'lightgray',
-                    borderStyle : 'dashed',
-                    borderWidth : '2px',
-                    borderColor : 'gray',
-                    borderRadius : '8px',
-                    marginTop : '5px'
-                  }}>
+                <div id="image-upload-div">
                   
                   <img style={{width: '50px', height: '50px', backgroundColor: 'yellow'}}/>
                   <p>Upload a clear photo of your room</p>
                   <IonButton color="light"> Choose Photo </IonButton>
                   
                 </div>
-    
-                <h2> Room Type</h2>
-                <DropdownMenu label="Select Room Type"></DropdownMenu>
-    
-                <h2>Design Style (Optional)</h2>
-                <DropdownMenu label="Select Style"></DropdownMenu>
-    
-                <IonButton color="dark">
-                  Next
-                </IonButton>
-    
+
+                <div id="controls-div">
+
+                  <div className="input-div">
+                    <span>Room Type</span>
+                    <IonList>
+                      <IonItem>
+                        <IonSelect placeholder="Favorite Fruit" interface="popover">
+                          <IonSelectOption value="apple">Apple</IonSelectOption>
+                          <IonSelectOption value="banana">Banana</IonSelectOption>
+                          <IonSelectOption value="orange">Orange</IonSelectOption>
+                        </IonSelect>
+                      </IonItem>
+                    </IonList>
+                  </div>
+      
+                  <div className="input-div">
+                    <span>Design Style (Optional)</span>
+                    <IonList>
+                      <IonItem>
+                        <IonSelect placeholder="Favorite Fruit" interface="popover">
+                          <IonSelectOption value="apple">Apple</IonSelectOption>
+                          <IonSelectOption value="banana">Banana</IonSelectOption>
+                          <IonSelectOption value="orange">Orange</IonSelectOption>
+                        </IonSelect>
+                      </IonItem>
+                    </IonList>
+                  </div>
+      
+                  <IonButton color="dark">
+                    Next
+                  </IonButton>
+                </div>
+
+                <span>You can customize more later</span>
+                <IonButton> Next </IonButton>
+
             </div>
+
         
       </IonContent>
     </IonPage>

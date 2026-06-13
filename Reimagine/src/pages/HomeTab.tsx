8bf604a3d211b804bef3865c3b411ebdd388cf69
Matthew Drake <matthewdrake2004@gmail.com>
2026-06-13 11:20:26 -0500

    Mocked up the structure for the first home tab screen

--- Reimagine/src/pages/HomeTab.tsx
+++ Reimagine/src/pages/HomeTab.tsx
@@ -1,23 +1,56 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonButton, IonButtons, IonContent, IonHeader, IonMenu, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 
 const HomeTab: React.FC = () => {
   return (
-    <IonPage>
+    <>
+
+    {/* Menu Contents */}
+    <IonMenu contentId="home-content">
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Menu Content</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding">This is the menu content.</IonContent>
+    </IonMenu>
+
+    {/* Page Contents */}
+    <IonPage id="home-content">
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle size="large"> Home </IonTitle>
+          <IonButtons slot="start">
+            <IonMenuButton></IonMenuButton>
+          </IonButtons>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+
+        {/* Content */}
+        <div style={{display: 'flex' , flexDirection : 'column', alignItems: 'center', width: '100vw', height: '100vh'}}>
+
+            <img style={{width : '95%', height: '40%', backgroundColor: 'yellow', marginTop: '10px'}}/>
+
+
+            <div style={{display : 'flex', flexDirection : 'column', width : '30%'}}>
+              
+              <h1 style={{fontWeight : 'bold'}}> Redesign your space with AI</h1>
+              <p>Upload a photo of your room and let AI create stunning designs for you.</p>
+              
+              <IonButton color="dark">
+                Upload Room Photo
+              </IonButton>
+
+              <IonButton color="light"> Start Designing </IonButton>
+            </div>
+        </div>
+
+
+        
       </IonContent>
     </IonPage>
+    
+    </>
   );
 };
 
